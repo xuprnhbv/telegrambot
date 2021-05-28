c240467d06ebf5f5c91d938f0ce425cf9340f9d9
Ido <Ido@DESKTOP-M5BOPQ7>
2021-05-28 07:17:36 +0300

    Moved relative token path to consts from main

--- consts.py
+++ consts.py
@@ -3,6 +3,7 @@ from colorama import Fore
 TEXT_COLOR = Fore.LIGHTWHITE_EX
 DAILY_MEME_HOUR = '06:30'
 MEMES_PATH = r"../videos/"
+RELATIVE_TOKEN_PATH = r'../token.key'
 BOKER_TOV = 'בוקר טוב '
 SEND_MEME_TO = [
     -455126807,  # 008
