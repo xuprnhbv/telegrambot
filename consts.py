86494939da2b4b4635e8c25ea26fb6ab9c21ac54
Ido <Ido@DESKTOP-M5BOPQ7>
2021-07-16 16:57:06 +0300

    Moved log to ./../botlog instead of src folder

--- consts.py
+++ consts.py
@@ -7,7 +7,7 @@ RELATIVE_TOKEN_PATH = r'../token.key'
 BOKER_TOV = 'בוקר טוב '
 MANAGEMENT_CHAT = -1001413795548
 DATE_REGEX = r'^[0-9]{8}-[0-9]{6}$'
-LOG_PATH = r'./botlog'
+LOG_PATH = r'./../botlog'
 SEND_MEME_TO = [
     -455126807,  # 008
 ]
