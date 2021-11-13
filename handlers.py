4282dc87f26ee0999584637e522ed00f73a382ee
ido moshe <www.ido.co.il@gmail.com>
2021-11-13 18:49:22 +0200

    Fucking ifs

--- handlers.py
+++ handlers.py
@@ -161,7 +161,7 @@ def get_chat_ids(update, context):
 
 def subscribe_to_memes(update, context):
     chats = _get_chats()
-    if update.effective_chat.id in chats:
+    if update.effective_chat.id in chats.keys():
         context.bot.send_message(chat_id=update.effective_chat.id, text="You're already subscribed!")
         return
     chats.update({update.effective_chat.id: update.effective_chat.username})
@@ -174,7 +174,7 @@ def subscribe_to_memes(update, context):
 
 def unsubscribe_to_memes(update, context):
     chats = _get_chats()
-    if update.effective_chat.id in chats:
+    if update.effective_chat.id in chats.keys():
         context.bot.send_message(chat_id=update.effective_chat.id, text='You are not subscribed!')
         return
     chats.pop(str(update.effective_chat.id))
