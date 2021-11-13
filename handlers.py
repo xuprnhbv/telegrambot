d6ee554a974464a0e76c7144f1dd51cc1a2c55c4
ido moshe <www.ido.co.il@gmail.com>
2021-11-13 18:43:16 +0200

    aaaaaaaaaaaaa

--- handlers.py
+++ handlers.py
@@ -174,7 +174,7 @@ def subscribe_to_memes(update, context):
 
 def unsubscribe_to_memes(update, context):
     chats = _get_chats()
-    if update.effective_chat.id in list(chats):
+    if update.effective_chat.id in chats:
         context.bot.send_message(chat_id=update.effective_chat.id, text='You are not subscribed!')
         return
     chats.pop(str(update.effective_chat.id))
