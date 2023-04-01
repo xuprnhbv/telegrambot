df9a5aa19ee57c617d7e5ba01277b501d0c99c40
Ido <Ido@DESKTOP-M5BOPQ7>
2023-04-01 18:00:05 +0300

    fix reply_text to edit_message

--- handlers.py
+++ handlers.py
@@ -227,7 +227,8 @@ def get_inline_handlers():
 
 #### Bot ####
 def main_inline_menu(update, _):
-    update.message.reply_text('Choose an option to begin.', reply_markup=main_keyboard(update.effective_chat))
+    update.callback_query.message.edit_text('Choose an option to begin.',
+                                            reply_markup=main_keyboard(update.effective_chat))
 
 
 def files_inline_menu(update, context):
