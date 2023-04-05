420f408585affcf0316892c2b1d930e4c9f9fd36
wwwid <www.ido.co.il@gmail.com>
2023-04-05 14:39:09 +0300

    removing old command line handlers

--- handlers.py
+++ handlers.py
@@ -4,7 +4,7 @@ from colorama import Fore
 from files import download_meme, delete_meme, MEMES_PATH
 from dailymeme import send_random_meme, choose_next_meme, get_chat_list, is_next_meme_chosen, get_chosen_meme
 from chats import _get_chats
-from consts import TEXT_COLOR, MANAGEMENT_CHAT, DATE_REGEX, EFI_ID, CHAT_IDS_PATH, HELP_OP, MANAGEMENT_HELP_OP, \
+from consts import TEXT_COLOR, MANAGEMENT_CHAT, DATE_REGEX, EFI_ID, CHAT_IDS_PATH, \
     INLINE_REGEX
 import logger
 import re
@@ -28,15 +28,6 @@ def add_all_handlers(updater: Updater):
     dispatcher = updater.dispatcher
     handler_arr = [
         MessageHandler(Filters.video & Filters.chat(MANAGEMENT_CHAT), save_meme),
-        CommandHandler('help', help),
-        CommandHandler('rm', remove_meme, filters=Filters.chat(MANAGEMENT_CHAT)),
-        CommandHandler('listmemes', listdir, filters=Filters.chat(MANAGEMENT_CHAT)),
-        CommandHandler('forcesend', force_send_meme, filters=Filters.chat(MANAGEMENT_CHAT)),
-        CommandHandler('version', get_version, filters=Filters.chat(MANAGEMENT_CHAT)),
-        CommandHandler('listchats', get_chat_ids, filters=Filters.chat(MANAGEMENT_CHAT)),
-        CommandHandler('subscribe', subscribe_to_memes),
-        CommandHandler('unsubscribe', unsubscribe_to_memes),
-        CommandHandler('rmchat', kick_from_memes, filters=Filters.chat(MANAGEMENT_CHAT)),
         MessageHandler(Filters.regex(r'([cC][sS])+') & (~Filters.command), at_efi),
     ]
 
@@ -54,17 +45,7 @@ def add_all_handlers(updater: Updater):
     return failed_handlers
 
 
-def help(update, context):
-    msg = HELP_OP
-    if update.effective_chat.id == MANAGEMENT_CHAT:
-        msg += f'\n{MANAGEMENT_HELP_OP}'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
-
-
-def force_send_meme(update, context):
-    logger.print_log('{text}Force sending meme...'.format(text=TEXT_COLOR))
-    send_random_meme(context)
-
+################### NON INLINE HANDLERS #############################
 
 def save_meme(update, context):
     logger.print_log('{text}Saving meme...'.format(text=TEXT_COLOR))
@@ -82,130 +63,13 @@ def save_meme(update, context):
         context.bot.send_message(chat_id=MANAGEMENT_CHAT, text='Saved successfully as {}'.format(filename))
 
 
-def remove_meme(update, context):
-    if len(context.args) != 1:
-        context.bot.send_message(chat_id=MANAGEMENT_CHAT, text='Please specify date of file to remove.')
-        return
-
-    datestr = context.args[0]
-    logger.print_log('{text}Removing meme...'.format(text=TEXT_COLOR))
-
-    if not re.search(DATE_REGEX, datestr):
-        logger.print_log(
-            '{red}Bad input {}. Does not match regex.{text}'.format(datestr, red=Fore.LIGHTRED_EX, text=TEXT_COLOR))
-        context.bot.send_message(chat_id=MANAGEMENT_CHAT, text='Write date in format {} like specified when '
-                                                               'uploaded initially.'.format(DATE_REGEX))
-        return
-
-    did_delete = False
-    for filename in os.listdir(MEMES_PATH):
-        if filename.startswith(datestr):
-            try:
-                delete_meme(filename)
-                logger.print_log(
-                    '{text}Deleted file {yellow}{}{text}'.format(filename, yellow=Fore.LIGHTYELLOW_EX, text=TEXT_COLOR))
-                context.bot.send_message(chat_id=MANAGEMENT_CHAT, text='Deleted {} successfully.'.format(filename))
-                did_delete = True
-                break
-            except Exception as e:
-                logger.print_log('{error}Exception raised: {}'.format(str(e), error=Fore.LIGHTRED_EX))
-                context.bot.send_message(chat_id=MANAGEMENT_CHAT, text='{error}Exception raised: {}'.format(str(e),
-                                                                                                            error=Fore.LIGHTRED_EX))
-                break
-
-    if not did_delete:
-        logger.print_log(
-            '{text}No file starts with {red}{}{text}'.format(datestr, red=Fore.LIGHTRED_EX, text=TEXT_COLOR))
-        context.bot.send_message(chat_id=MANAGEMENT_CHAT, text='No such file {}'.format(datestr))
-
-
-def listdir(update, context):
-    logger.print_log('{text}Listing memes dir...'.format(text=TEXT_COLOR))
-    meme_dir = os.listdir(MEMES_PATH)
-    memes = f'Memes directory: {len(meme_dir)}\n'
-    for filename in meme_dir:
-        memes += '>' + filename + '\n'
-    context.bot.send_message(chat_id=MANAGEMENT_CHAT, text=memes)
-
-
-def resend_vid(update, context):
-    logger.print_log('Ran resend_vid')
-    context.bot.send_video(chat_id=update.effective_chat.id, caption='this was what you sent right? and the text w'
-                                                                     'as {}'.format(update.message.caption),
-                           video=update.message.effective_attachment)
-
-
 def at_efi(update, context):
     logger.print_log('{text}@ing Efi...'.format(text=TEXT_COLOR))
     context.bot.send_message(chat_id=update.effective_chat.id, text=f'[@Ofir](tg://user?id={EFI_ID})',
                              parse_mode="Markdown")
 
 
-def get_version(update, context):
-    repo = git.Repo(search_parent_directories=True)
-    current_ver_date = time.ctime(repo.head.commit.committed_date)
-    current_ver_sha = repo.head.commit.hexsha
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=f"Branch: {repo.active_branch.name}\nLast Commit Date: "
-                                  f"{current_ver_date}\nCommit SHA: {current_ver_sha}")
-
-
-def get_chat_ids(update, context):
-    chats = _get_chats()
-    msg = "Chats that receive daily memes:\n"
-    for cid in chats:
-        msg += f">> {cid} ({chats[cid]})\n"
-    context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
-
-
-def subscribe_to_memes(update, context):
-    chats = _get_chats()
-    if str(update.effective_chat.id) in chats.keys():
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You're already subscribed!")
-        return
-
-    name = update.effective_chat.username or update.effective_chat.title
-
-    chats.update({update.effective_chat.id: name})
-    with open(CHAT_IDS_PATH, 'w') as fd:
-        json.dump(chats, fd)
-    context.bot.send_message(chat_id=MANAGEMENT_CHAT, text=f"!!Added {update.effective_chat.type} to daily memes: "
-                                                           f"{name}"
-                                                           f" ({update.effective_chat.id})!!")
-    context.bot.send_message(chat_id=update.effective_chat.id, text="You've subscribed successfully!")
-
-
-def unsubscribe_to_memes(update, context):
-    chats = _get_chats()
-    if str(update.effective_chat.id) not in chats.keys():
-        context.bot.send_message(chat_id=update.effective_chat.id, text='You are not subscribed!')
-        return
-    chats.pop(str(update.effective_chat.id))
-    with open(CHAT_IDS_PATH, 'w') as fd:
-        json.dump(chats, fd)
-    name = update.effective_chat.username or update.effective_chat.title
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Unsubscribed successfully")
-    context.bot.send_message(chat_id=MANAGEMENT_CHAT, text=f"!!Removed {name} from "
-                                                           f"daily meme chats")
-
-
-def kick_from_memes(update, context):
-    if len(context.args) != 1:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Use it as /rmchat <chat id>")
-        return
-    chats = _get_chats()
-    rmchat_id = context.args[0]
-    if rmchat_id not in chats.keys():
-        context.bot.send_message(chat_id=update.effective_chat.id, text='Not a valid subbed chat.')
-        return
-    chats.pop(rmchat_id)
-    with open(CHAT_IDS_PATH, 'w') as fd:
-        json.dump(chats, fd)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Successfully removed {rmchat_id}"
-                                                                    f" from daily meme chats.")
-
-
-################### MENUS #############################
+################### INLINE #############################
 
 #### Handler Initiation ####
 def get_inline_handlers():
@@ -234,8 +98,9 @@ def get_inline_handlers():
 #### Bot ####
 def main_inline_menu(update, _):
     try:
-        update.callback_query.message.edit_text(f'Choose an option to begin, {update.effective_message.from_user.username}.',
-                                                reply_markup=main_keyboard(update.effective_chat))
+        update.callback_query.message.edit_text(
+            f'Choose an option to begin, {update.effective_message.from_user.username}.',
+            reply_markup=main_keyboard(update.effective_chat))
     except AttributeError:
         update.message.reply_text(f'Choose an option to begin, {update.effective_message.from_user.username}.',
                                   reply_markup=main_keyboard(update.effective_chat))
@@ -323,7 +188,7 @@ def force_send_now_inline(update, _):
                                                 InlineKeyboardButton('Yes',
                                                                      callback_data=f'fsn@_@yes@_@{meme_to_send}'),
                                                 InlineKeyboardButton('No', callback_data=f'fsn@_@no@_@{meme_to_send}' if
-                                                                     meme_to_send else "main_menu")
+                                                meme_to_send else "main_menu")
                                             ]]))
 
 
