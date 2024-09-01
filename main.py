13edee6e4b90a9fc70f741757489765d135adc19
wwwid <www.ido.co.il@gmail.com>
2024-09-01 09:29:13 +0300

    added more necessary awaits

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def get_token():
 
 def main(bg):
     token_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), RELATIVE_TOKEN_PATH))
-    logger.log_setup(print_console=True)
+    logger.log_setup(print_console=bg)
     logger.print_log('Getting bot token from {}...'.format(token_path))
     token = get_token()
     logger.print_log('Token: {}'.format(token))
@@ -28,14 +28,11 @@ def main(bg):
     logger.print_log('Adding handlers...')
     add_all_handlers(app)
 
-    logger.print_log('Starting the bot...')
-    app.run_polling()
     logger.print_log('Adding daily meme task...')
     daily_meme_thread = threading.Thread(target=init_daily_meme, args=[app], daemon=True)
     daily_meme_thread.start()
     logger.print_log('Meme will be sent everyday at {}.'.format(DAILY_MEME_HOUR))
-    app.idle()
-
+    app.run_polling()
 
 if __name__ == '__main__':
     main("bg" in sys.argv)
